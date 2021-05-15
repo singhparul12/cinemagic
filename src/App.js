2b48daaeedafa450ac2e064ebf0acdf5ebab061f
singhparul12 <singhparul120999@gmail.com>
2021-05-15 14:54:46 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SearchMovies from './components/SearchMovies';
+import './components/styles1.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export default () => {
+    return (
+         <div>
+             <h1>CINEMAGIC</h1>
+             <SearchMovies /> 
+        </div>
+    );
+};
